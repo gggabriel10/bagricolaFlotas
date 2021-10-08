00be2cfe678dd45d6e455831adb25f5a459ee5d0
Gabriel-10 <gabygarciag10@gmail.com>
2021-10-08 09:45:43 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+class config:
+    orm_mode = True
+
+@app.get("/")
+async def root():
+    return{'Sistema': 'Bagricola Flotas'}
+
+@app.get("/api/ObtenerDatos")
+async def obtenerDatos():
+    try:
+        datos = []
+        conexion = sqlite3.connect("DatosFlotas.db")
+        cursor = conexion.cursor()
+        cursor.execute("SELECT ID, Numero, Nombre FROM Flotas")
+        contenido = cursor.fetchall()
+        conexion.commit()
+        for i in contenido:
+            datos.append({"Id":i[0],"Numero":i[1], "Nombre":i[2]})
+        return datos
+    except TypeError:
+        return "ERROR AL CONECTAR CON LA BASE DE DATOS"  
